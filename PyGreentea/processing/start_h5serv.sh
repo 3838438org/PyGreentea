911c0348a9ebf93d366d6d430159262d816fcaf1
William Grisaitis <grisaitisw@janelia.hhmi.org>
2016-10-27 15:53:13 -0400

    Adjust HDF5 server settings

--- PyGreentea/processing/start_h5serv.sh
+++ PyGreentea/processing/start_h5serv.sh
@@ -1,7 +1,7 @@
 docker run \
-    --rm \
+    -d \
     -u `id -u $USER` \
     -p 5000:5000 \
-    -v /scratch/affinities/fib25:/data \
+    -v /groups/turaga/home/grisaitisw/data:/data \
     --name "h5serv" \
     hdfgroup/h5serv
