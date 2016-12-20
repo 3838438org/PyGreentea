0e77edeb8906fd1cc8842f930b01c8182a593022
William Grisaitis <grisaitisw@janelia.hhmi.org>
2016-12-20 16:43:43 -0500

    OffsettedArray can be sliced with slice objects, integers, or None

--- data_io/offsetted_array.py
+++ data_io/offsetted_array.py
@@ -1,3 +1,9 @@
+from __future__ import print_function
+
+import collections
+import numpy as np
+
+
 class OffsettedArray(object):
     def __init__(self, array_like, offset, shape):
         assert len(array_like.shape) == len(offset), (array_like.shape, offset)
@@ -7,21 +13,45 @@ class OffsettedArray(object):
         self.shape = shape
         self.dtype = self.array_like.dtype
 
+    @staticmethod
+    def translate_index_expression(index_expression, dim_offset, dim_length):
+        if type(index_expression) is int:
+            # translate the location
+            offsetted_index_expression = index_expression + dim_offset
+        elif type(index_expression) is slice:
+            # translate the slice
+            start = (index_expression.start or 0) + dim_offset
+            stop = (index_expression.stop or 0) + dim_offset
+            offsetted_index_expression = np.s_[start:stop]
+        elif index_expression is None:
+            # get everything
+            start = dim_offset
+            stop = dim_offset + dim_length
+            offsetted_index_expression = np.s_[start:stop]
+        else:
+            raise TypeError("OffsettedArray only works with "
+                            "index expressions of type slice, int or NoneType")
+        return offsetted_index_expression
+
     def _convert_item(self, item):
-        new_item = list(item)
-        for i, s in enumerate(item):
-            assert type(s) is slice, "sorry... OffsettedArray only works with slice objects"
-            if i >= len(self.array_like.shape) - len(self.offset):
-                new_item[i] = slice(s.start + self.offset[i], s.stop + self.offset[i], s.step)
-        return tuple(new_item)
+        if not isinstance(item, collections.Iterable):
+            # make sure item is iterable
+            item = (item,)
+        offsetted_item = list(item)
+        if Ellipsis in item:
+            raise TypeError("Indexing with an Ellipsis is not supported by OffsettedArray")
+        for dim_index, index_expression in enumerate(item):
+            dim_offset = self.offset[dim_index]
+            dim_length = self.shape[dim_index]
+            offsetted_index_expression = self.translate_index_expression(index_expression, dim_offset, dim_length)
+            offsetted_item[dim_index] = offsetted_index_expression
+        return tuple(offsetted_item)
 
     def __getitem__(self, item):
         new_item = self._convert_item(item)
-        print("fetching item", new_item)
         return self.array_like[new_item]
 
     def __setitem__(self, item, value):
         new_item = self._convert_item(item)
-        print("setting item", new_item)
         self.array_like[new_item] = value
         return
